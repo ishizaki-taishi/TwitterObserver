68dac092badc0af14356ce14d9431ba693339b5e
tenonno <hypertis0402@gmail.com>
2017-08-08 08:49:54 +0900

    socket.io & pg

--- main.js
+++ main.js
@@ -1,15 +1,19 @@
-var express = require('express');
-var app = express();
-
 const port = process.env.PORT || 3000;
 
-app.get('/', function(req, res) {
-    res.send('Hello World!');
-});
+const app = require('express')();
+const http = require('http').Server(app);
 
-app.listen(port, function() {
-    console.log('Example app listening on port 3000!');
-});
+
+
+
+const io = require('socket.io')(http);
+
+io.listen(http);
+
+
+const path = require('path');
+
+const databaseURL = 'postgres://zsmdfzjfczrdyi:16cdebdfc49073acbdb90c47a098d14bdd4a6bf5d6ee3ca31e0ee5c3c49e4804@ec2-54-221-221-153.compute-1.amazonaws.com:5432/d4k46sqvuojehi';
 
 
 
@@ -23,14 +27,121 @@ const client = new Twitter({
 });
 
 
+io.sockets.on('connection', (socket) => {
 
-const params = { screen_name: 'nodejs' };
 
-client.get('statuses/user_timeline', params, function(error, tweets, response) {
+    socket.on('ss', (id) => {
 
-    console.log(arguments)
+        let params = {
+            id
+        };
 
-    if (!error) {
-        console.log(tweets);
-    }
+
+        /*
+
+
+            */
+        /*
+                client.get('statuses/show', params, (error, tweets, response) => {
+
+                    io.emit('log', { error, tweets, response });
+
+                });
+
+                */
+
+        params.stringify_ids = true;
+
+        client.get('statuses/retweeters/ids', params, async(e, t, r) => {
+            try {
+
+                if (e) return console.error(e);
+
+
+                for (const id of t.ids.slice(0, 5)) {
+
+
+                    const t = await get('users/show', { id });
+
+                    const { name, screen_name } = t;
+
+                    io.emit('log', `${name} @${screen_name}`);
+
+                }
+
+
+
+
+                io.emit('log', { e, t, r });
+
+            } catch (e) {
+                io.emit('error', e);
+            }
+        });
+
+
+
+    });
+
+
+    return;
+    socket.on('msg', function(data) {
+        io.sockets.emit('msg', data);
+    });
 });
+
+function get(api, params) {
+    return new Promise((resolve, reject) => {
+        client.get(api, params, (e, t, r) => {
+            if (e) {
+                console.error(e);
+                reject(e);
+            }
+            resolve(t);
+        });
+    });
+}
+
+
+app.get('/', async(req, res) => {
+    res.sendFile(path.resolve(__dirname, 'index.html'));
+});
+
+http.listen(port, function() {
+    console.log('Example app listening on port 3000!');
+});
+
+
+
+
+
+
+(() => {
+
+
+    const { Pool, Client } = require('pg')
+    const connectionString = process.env.DATABASE_URL || databaseURL; //'postgresql://dbuser:secretpassword@database.server.com:3211/mydb'
+
+    io.emit('log', connectionString);
+
+    const pool = new Pool({
+        connectionString: connectionString,
+        ssl: true
+    });
+
+
+
+    const client = new Client({
+        connectionString: connectionString,
+        ssl: true
+
+    });
+
+    client.connect()
+
+    client.query('SELECT NOW()', (err, res) => {
+        console.log(err, res);
+        client.end();
+    });
+
+})();
