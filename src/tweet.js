039ce86834d523b464a176838fc176f99a6c8cd7
Ishizaki Taishi <ishizaki.taishi@donuts.ne.jp>
2017-08-18 20:59:31 +0900

    webpack 用にファイルを整理した

---
+++ src/tweet.js
@@ -0,0 +1,37 @@
+export default class Tweet {
+
+    constructor() {
+
+        this.id = Math.random();
+        this.retweeters = [];
+
+    }
+
+    static from(data) {
+
+        const tweet = new Tweet();
+
+        tweet.id = data.id;
+        tweet.spreadsheetId = null; // data.spreadsheet_id;
+        tweet.oembed = data.oembed;
+
+        tweet.view = {
+
+            isShowOptions: false,
+
+            ffRatioCheck: false,
+            ffRatioBorder: null,
+
+            followerCountCheck: false,
+            followerCountBorder: null,
+
+            time: null
+
+
+        };
+
+        return tweet;
+
+    }
+
+}
